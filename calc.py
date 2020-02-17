dc37e633255e938a9e1cf217a68574b03292e5f7
root <root@ip-172-31-12-244.us-east-2.compute.internal>
2020-02-17 07:53:39 +0000

    second

---
+++ calc.py
@@ -0,0 +1,13 @@
+def add(x.y):
+  “”"Add Function”“”
+  return x+y
+def sub(x,y):
+  “”"Subtract Funtion”“”
+  return x-y
+def multiply(x,y):
+  “”"Multiply Funtion”“”
+  return x*y
+def divide(x,y):
+  “”"Divide Funtion”“”
+  return x/y
+
